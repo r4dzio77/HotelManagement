f8a96ad609096b5c3766fcf4999249abe5970a2b
r4dzio77 <oradziewicz@interia.pl>
2025-07-08 19:06:07 +0200

    Podstawowe ustawienia konta

---
+++ HotelManagement/Migrations/20250708165554_AddPreferencesToApplicationUser.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HotelManagement.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPreferencesToApplicationUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Preferences",
+                table: "AspNetUsers",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Preferences",
+                table: "AspNetUsers");
+        }
+    }
+}
