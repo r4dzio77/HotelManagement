ed89d8bbc861fb8fd9385cc0de3033458ae8925f
Julia <91841@student.pb.edu.pl>
2025-08-28 05:36:57 +0200

    Poprawienie widoku rozliczania, eksport jako PDF paragonow i faktur

--- HotelManagement/Migrations/20250827221428_AddBuyerInfoToDocument.Designer.cs
+++ HotelManagement/Migrations/20250827221428_AddBuyerInfoToDocument.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using HotelManagement.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace HotelManagement.Migrations
 {
     [DbContext(typeof(HotelManagementContext))]
-    partial class HotelManagementContextModelSnapshot : ModelSnapshot
+    [Migration("20250827221428_AddBuyerInfoToDocument")]
+    partial class AddBuyerInfoToDocument
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.13");
@@ -211,10 +214,6 @@ namespace HotelManagement.Migrations
                         .HasColumnType("TEXT");
 
                     b.Property<DateTime>("IssueDate")
-                        .HasColumnType("TEXT");
-
-                    b.Property<string>("Number")
-                        .IsRequired()
                         .HasColumnType("TEXT");
 
                     b.Property<int>("ReservationId")
---
+++ HotelManagement/Migrations/20250827221428_AddBuyerInfoToDocument.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HotelManagement.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddBuyerInfoToDocument : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "BuyerAddress",
+                table: "Documents",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "BuyerName",
+                table: "Documents",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "BuyerNip",
+                table: "Documents",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BuyerAddress",
+                table: "Documents");
+
+            migrationBuilder.DropColumn(
+                name: "BuyerName",
+                table: "Documents");
+
+            migrationBuilder.DropColumn(
+                name: "BuyerNip",
+                table: "Documents");
+        }
+    }
+}
