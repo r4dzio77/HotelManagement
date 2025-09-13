21eee511583c348b4ec807a8f491c347e92b7086
Julia <91841@student.pb.edu.pl>
2025-09-13 04:02:27 +0200

    Kosmetyczne poprawki - grafik oraz widok bookingowy

---
+++ HotelManagement/Migrations/20250910002557_AddShiftHours.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HotelManagement.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddShiftHours : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<TimeSpan>(
+                name: "EndHour",
+                table: "WorkShifts",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<TimeSpan>(
+                name: "StartHour",
+                table: "WorkShifts",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EndHour",
+                table: "WorkShifts");
+
+            migrationBuilder.DropColumn(
+                name: "StartHour",
+                table: "WorkShifts");
+        }
+    }
+}
